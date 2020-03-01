5cc69b43dacd7b4e4d47f99896da5a91d69726e3
Nathan Luu <nate@Nathans-MBP.local>
2020-03-01 15:42:58 -0800

    Init React app

---
+++ front-end/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Side menu */}
+
+      <h1>JobHunter</h1>
+    </div>
+  );
+}
+
+export default App;
